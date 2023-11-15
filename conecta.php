45028ba72b92708601c1a0ba9122a7bf3d289175
Ismael <ismaelbd042@gmail.com>
2023-11-15 10:01:27 +0100

    Establecemos conexión con la base de datos

--- conecta.php
+++ conecta.php
@@ -1,1 +1,7 @@
 <?php
+// Establecemos la conexión con BD:
+$servidor = "localhost";
+$usuario = "root";
+$password = "";
+$conexion = mysqli_connect($servidor, $usuario, $password) or die("Error de conexión");
+mysqli_select_db($conexion, "usuarios");
