7839988acf38de060095176f99dba5d32cb8a9c3
Ismael <ismaelbd042@gmail.com>
2023-11-15 10:58:53 +0100

    Creamos las tablas lectores y prestamo

--- crea_tablas.php
+++ crea_tablas.php
@@ -1,1 +1,19 @@
 <?php
+require_once "conecta.php";
+
+// Creamos tabla lectores con sus respectivos campos
+$lectores = "CREATE TABLE IF NOT EXISTS lectores (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    lector VARCHAR(100) NOT NULL,
+    DNI VARCHAR(9) NOT NULL,
+    estado VARCHAR(30) NOT NULL,
+    n_prestado INT NOT NULL,
+)";
+$conn->query($sql_lectores);
+
+// Crear tabla prestamo
+$prestamo = "CREATE TABLE IF NOT EXISTS prestamo (
+    id_lector INT,
+    id_libro INT,
+)";
+$conn->query($sql_lectores);
