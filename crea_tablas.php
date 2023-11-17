926be0f000c85b68ea410b9b34896d1ea04aa703
Pablo <pablonaranjocid@gmail.com>
2023-11-17 09:14:29 +0100

    Creamos tabla libros

--- crea_tablas.php
+++ crea_tablas.php
@@ -1,1 +1,13 @@
 <?php
+require_once "conecta.php";
+
+$libros = "CREATE TABLE IF NOT EXISTS libros (
+   id INT AUTO_INCREMENT PRIMARY KEY,
+   nombre VARCHAR(50) NOT NULL,
+   autor VARCHAR(50) NOT NULL,
+   publicacion DATE NOT NULL,
+   ISBN VARCHAR(13) NOT NULL,
+   sinopsis VARCHAR(500) NOT NULL,
+   n_disponibles INT NOT NULL,
+   n_totales INT NOT NULL,
+                )";
