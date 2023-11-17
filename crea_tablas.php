b3cec3d8469df47e566ec4e620aac71d9fb52621
Pablo <pablonaranjocid@gmail.com>
2023-11-17 11:58:57 +0100

    Código totalmente nuevo

--- crea_tablas.php
+++ crea_tablas.php
@@ -9,7 +9,7 @@ $libros = "CREATE TABLE IF NOT EXISTS libros (
    ISBN VARCHAR(13) NOT NULL,
    sinopsis VARCHAR(500) NOT NULL,
    n_disponibles INT NOT NULL,
-   n_totales INT NOT NULL,
+   n_totales INT NOT NULL
 )";
 
 // Creamos tabla lectores con sus respectivos campos
@@ -17,28 +17,28 @@ $lectores = "CREATE TABLE IF NOT EXISTS lectores (
     id INT AUTO_INCREMENT PRIMARY KEY,
     lector VARCHAR(100) NOT NULL,
     DNI VARCHAR(9) NOT NULL,
-    estado VARCHAR(30) NOT NULL,
-    n_prestado INT NOT NULL,
+    estado ENUM('alta', 'baja') DEFAULT 'alta',
+    n_prestado INT NOT NULL
 )";
 
 // Crear tabla prestamo
 $prestamo = "CREATE TABLE IF NOT EXISTS prestamo (
     id_lector INT,
-    id_libro INT,
+    id_libro INT
 )";
 
-mysqli_query($conexion, $libros) or die("Error al crear la tabla libros: ");
-mysqli_query($conexion, $lectores) or die("Error al crear la tabla lectores: ");
-mysqli_query($conexion, $prestamo) or die("Error al crear la tabla prestamo: ");
+mysqli_query($conexion, $libros) or die("Error al crear la tabla libros");
+mysqli_query($conexion, $lectores) or die("Error al crear la tabla lectores");
+mysqli_query($conexion, $prestamo) or die("Error al crear la tabla prestamo");
 
 
-$insertar_clientes_iniciales = "INSERT INTO lectores (lector, DNI, estado, n_prestamo)
+$insertar_clientes_iniciales = "INSERT INTO lectores (lector, DNI, estado, n_prestado)
                                  VALUES ('Pablo', '54242131N', 'alta', '2'),
-                                 VALUES ('Imael', '02571143L', 'alta', '1')";
+                                 ('Ismael', '02571143L', 'alta', '1')";
 
 $insertar_prestamo_inicial = "INSERT INTO prestamo (id_lector, id_libro)
                                  VALUES ('1', '2'),
-                                 VALUES ('2', '1')";
+                                 ('2', '1')";
 
 $insertar_libros_iniciales = "INSERT INTO libros (nombre, autor, publicacion, ISBN, sinopsis, n_disponibles, n_totales)
                             VALUES ('El ingenioso hidalgo don Quijote de la Mancha', 'Miguel de Cervantes', '1605', '9788408061052', 
