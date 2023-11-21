eee5052736a06a39ec2ea7694a084a5f61abfdb5
Ismael <ismaelbd042@gmail.com>
2023-11-21 09:29:43 +0100

    Terminamos funcion realizar_prestamo

--- procesar.php
+++ procesar.php
@@ -10,6 +10,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
          case 'registrar_lector':
             registrar_lector($conexion); // Pasar $conexion como argumento
             break;
+         case 'realizar_prestamo':
+            realizar_prestamo($conexion); // Pasar $conexion como argumento
+            break;
          default:
             break;
       }
@@ -27,4 +30,26 @@ function registrar_lector($conexion)
    mysqli_query($conexion, $sql) or die("Error al insertar datos");
    // Cerrar la conexión
    $conexion->close();
+}
+
+function realizar_prestamo($conexion)
+{
+   $nombreLector = $_POST['nombre_lector_prestamo'];
+   $nombreLibro = $_POST['nombre_libro_prestamo'];
+
+   // Obtener el ID del libro
+   $sql1 = "SELECT id FROM libros WHERE nombre = '$nombreLibro'";
+   $resultado1 = mysqli_query($conexion, $sql1) or die("Error al select datos");
+   $fila1 = mysqli_fetch_assoc($resultado1);
+   $id_libro = $fila1['id'];
+
+   // Obtener el ID del lector
+   $sql2 = "SELECT id FROM lectores WHERE lector = '$nombreLector'";
+   $resultado2 = mysqli_query($conexion, $sql2) or die("Error al select datos");
+   $fila2 = mysqli_fetch_assoc($resultado2);
+   $id_lector = $fila2['id'];
+
+   // Realizar la inserción en la tabla prestamo
+   $insert = "INSERT INTO prestamo (id_lector, id_libro) VALUES ('$id_lector', '$id_libro')";
+   mysqli_query($conexion, $insert) or die("Error al insertar datos");
 }
